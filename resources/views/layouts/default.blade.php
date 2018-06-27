234fd9b7e71900f97bfc4650465d3c8970aa6431
eklers55 <40613005+eklers55@users.noreply.github.com>
2018-06-27 23:41:42 +0300

    ŗss feed
    added RSS feed

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,8 +5,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         
-        <title>{{config('app.name', 'STREAMS')}}</title>
-
+        <title>{{config('app.name', 'STREAMS')}}</title> 
+        {!!Feed::link(url('feed'), 'atom', 'My Feed')!!} 
         <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
